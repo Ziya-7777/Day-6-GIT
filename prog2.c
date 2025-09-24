6282cabbf6f8fdfb40eb7e110a0edcecf04aaa16
Ziya <ziyamalkani333@gmail.com>
2025-09-24 11:32:12 +0530

    first commit

---
+++ prog2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+
+    int x=10,y=20;
+    printf("square of x-y is: %d",x*x-2*x*y+y*y);
+    return 0;
+}
---
+++ prog3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+
+    int x=10,y=20;
+    printf("cube of x+y is: %d",x*x*x+3*x*x*y+3*x*y*y+y*y*y);
+    return 0;
+}
